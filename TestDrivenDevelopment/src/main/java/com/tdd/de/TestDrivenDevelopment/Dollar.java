51b9d27f29fc191d6e026e5df60a7ff6e050ae3c
Abhijeet Shetty <abhijeetshetty001@gmail.com>
2021-01-26 12:06:14 +0100

    Refactor: Added new class Money

--- TestDrivenDevelopment/src/main/java/com/tdd/de/TestDrivenDevelopment/Dollar.java
+++ TestDrivenDevelopment/src/main/java/com/tdd/de/TestDrivenDevelopment/Dollar.java
@@ -1,37 +1,13 @@
 package com.tdd.de.TestDrivenDevelopment;
 
-public class Dollar {
+public class Dollar extends Money {
 
-	private int amount;
 	Dollar(int amount){
 		this.amount = amount;
 	}
 	
 	Dollar times(int multiplier) {
 		return new Dollar( amount * multiplier);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + amount;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Dollar other = (Dollar) obj;
-		if (amount != other.amount)
-			return false;
-		return true;
-	}
-	
+	}	
 	
 }
--- TestDrivenDevelopment/src/main/java/com/tdd/de/TestDrivenDevelopment/Franc.java
+++ TestDrivenDevelopment/src/main/java/com/tdd/de/TestDrivenDevelopment/Franc.java
@@ -1,7 +1,6 @@
 package com.tdd.de.TestDrivenDevelopment;
 
-public class Franc {
-	private int amount;
+public class Franc extends Money{
 
 	Franc(int amount) {
 		this.amount = amount;
@@ -9,28 +8,6 @@ public class Franc {
 
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + amount;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Franc other = (Franc) obj;
-		if (amount != other.amount)
-			return false;
-		return true;
 	}
 	
 }
